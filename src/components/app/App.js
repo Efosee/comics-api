23171e9fad4ba4bdc1de52914c47a4a3aa0af8bb
Efosee <valeraparaskev@gmail.com>
2025-03-06 14:15:46 +0300

    fix bugs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import { MainPage, ComicsPage } from "../pages";
@@ -6,28 +6,23 @@ import { MainPage, ComicsPage } from "../pages";
 
 const App = () => {
 
-
-
 	return (
-		<Router>
+		<Router basename="/marvel">
 			<div className="app">
 				<AppHeader />
 				<main>
 					<Switch>
 						<Route exact path="/">
-							<MainPage/>
+							<MainPage />
 						</Route>
-
-						<Route exact path="/comics">
-							<ComicsPage/>
+						<Route path="/comics">
+							<ComicsPage />
 						</Route>
 					</Switch>
-
 				</main>
 			</div>
 		</Router>
 	)
-
 }
 
 export default App;
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,19 +1,19 @@
 import './appHeader.scss';
-import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, NavLink } from 'react-router-dom';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link href="/">
+                <Link to="/">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} href="/">Characters</NavLink></li>
+                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} to="/">Characters</NavLink></li>
                     /
-                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} href="/comics">Comics</NavLink></li>
+                    <li><NavLink exact activeStyle={{'color': '#9f0013'}} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
